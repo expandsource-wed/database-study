dbda25cdd72105d2674b6af962de54b1c87a31b1
expandsource-wed <expandsource@gmail.com>
2024-05-08 14:28:05 +0900

    ALTER 테이블 구조 및 제약조건 변경

--- 테이블생성/ALTER(테이블 구조 변경).sql
+++ 테이블생성/ALTER(테이블 구조 변경).sql
@@ -37,3 +37,81 @@ ALTER TABLE employees_ABC
     
 DESC employees;
 
+
+/* 제약조건 변경 */
+
+-- 고유 제약조건 추가하기
+ALTER TABLE employees
+	ADD COLUMN email VARCHAR(255);	-- 1. 일반 컬럼 추가
+
+ALTER TABLE employees
+	ADD UNIQUE (email);				-- 2. 고유 제약조건 추가
+    
+DESCRIBE employees;		-- 구조 확인
+
+-- 외래키 제약조건 추가하기
+-- 1. 참조할 테이블 만들기
+CREATE TABLE department (
+	department_id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(100)
+);
+
+-- 2. 외래키로 사용할 컬럼을 추가 (참조할 테이블의 기본키와 같은 타입)
+ALTER TABLE employees
+	ADD COLUMN department_id INT;
+    
+-- 3. 외래키 제약조건을 추가 (CONSTRAINT 제약조건의이름)
+ALTER TABLE employees
+	ADD CONSTRAINT fk_department_id
+    FOREIGN KEY (department_id) REFERENCES department(department_id);
+    
+DESCRIBE employees;
+
+
+-- 체크 제약조건 추가하기
+ALTER TABLE employees
+	ADD COLUMN age INT;		-- 1. 컬럼 추가하기
+    
+ALTER TABLE employees
+	ADD CONSTRAINT chk_age	-- 제약조건의 이름 명시
+    CHECK (age > 19);		-- 2. 체크 제약조건 추가
+
+DESCRIBE employees;
+
+-- 컬럼에 대한 설명(COMMENT) 추가하기
+ALTER TABLE employees
+	ADD COLUMN salary INT
+    COMMENT '직원이 받는 급여 컬럼입니다.';
+
+-- 제약조건 삭제하기
+-- 1. 외래키 삭제하기
+ALTER TABLE employees
+	DROP FOREIGN KEY fk_department_id;  -- 제약조건의 이름을 통해 삭제
+-- 2. 제약조건 삭제하기
+ALTER TABLE employees
+	DROP CONSTRAINT chk_age;
+
+-- ALTER 컬럼의 속성 변경하기 
+-- DEFAULT 제약조건 
+ALTER TABLE employees
+	ALTER COLUMN age SET DEFAULT 20;  -- 컬럼에 DEFAULT 추가(수정)
+    
+DESC employees;
+
+-- 기본값 제약조건 삭제
+ALTER TABLE employees
+	ALTER COLUMN age DROP DEFAULT;		-- 컬럼의 제약사항 변경
+
+-- 제약조건의 이름을 확인하기
+-- 1. 오브젝트를 통해 확인
+SELECT * FROM information_schema.table_constraints
+	WHERE table_name = 'employees';
+    
+-- 2. DDL 문을 통해서 확인
+SHOW CREATE TABLE employees;
+
+
+
+
+
+
